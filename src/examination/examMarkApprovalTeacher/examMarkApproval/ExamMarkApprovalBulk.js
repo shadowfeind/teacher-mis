a0fd94313a13e629f5fb092d6d18a33678b31b6d
amir0123 <amirkarmacharya4@gmail.com>
2022-02-20 16:30:51 +0545

    exam approval done

--- src/examination/examMarkApprovalTeacher/examMarkApproval/ExamMarkApprovalBulk.js
+++ src/examination/examMarkApprovalTeacher/examMarkApproval/ExamMarkApprovalBulk.js
@@ -19,7 +19,7 @@ import { postBulkExamMarkApprovalAction } from "./ExamMarkAprrovalActions";
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
-      backgroundColor: theme.palette.common.black,
+      backgroundColor: "#253053",
       color: theme.palette.common.white,
     },
     body: {
